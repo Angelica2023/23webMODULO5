beca0977164d712e7b6518e947be84678e4ea0f8
Angelica2023 <118866861+Angelica2023@users.noreply.github.com>
2023-06-15 19:34:04 -0600

    Add files via upload

---
+++ 04-giphy-app/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import ImageCard from './components/ImageCard'
+import SearchBar from './components/SearchBar'
+import './App.css'
+
+function App () {
+  const [gifs, setGifs] = useState([]) // Lista de Gifs 
+  const apiKey = import.meta.env.VITE_GIPHY_API_KEY
+  // Las llamadas a API normalmente se hacen en el useEffect
+  // Usaremos useEffect con un arreglo de dependencias vacío para que se ejecute solo una vez
+
+  const sendSearch = (search) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=25&offset=0&rating=g&lang=en`)
+      .then(response => response.json())
+      .then((results) => {
+        console.log(results.data)
+        setGifs(results.data)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }
+
+  useEffect(() => {
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=MNhdy5Hq4XDyh91F8bE4i36f6vi5u4U2&limit=25&offset=0&rating=g&bundle=messaging_non_clips`)
+      .then(response => response.json())
+      .then((results) => {
+        console.log(results.data)
+        setGifs(results.data)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }, [])
+//Las llamadas a la API se hacen normalmente a través de UseEffect
+//Se usara useEffect con un arreglo de dependencias vacio para que se ejecute una sola vez
+  return (
+    <>
+      
+      <div className='App'>
+        <SearchBar handleSearch={sendSearch} />
+
+        <div className='grid-cards'>
+          {
+            gifs.map(gif => (
+              <ImageCard
+                key={gif.id}
+                url={gif.images.fixed_height.url}
+                title={gif.title}
+              />
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
